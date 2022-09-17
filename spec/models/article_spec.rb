45203b7993aaac6a6eb91697b7bf550c1a6a5770
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-09-18 00:39:49 +0900

    article specの記述を確認し、問題がない事が確認できた

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -19,26 +19,25 @@
 #
 require "rails_helper"
 
-# RSpec.describe Article, type: :model do
-#   context "タイトルと本文に文字が入力されているとき" do
-#     binding.pry
-#     let(:article) { build(:article) }
-#     it "記事が作成される" do
-#       expect(article).to be_valid
-#     end
-#   end
+RSpec.describe Article, type: :model do
+  context "タイトルと本文に文字が入力されているとき" do
+    let(:article) { build(:article) }
+    it "記事が作成される" do
+      expect(article).to be_valid
+    end
+  end
 
-#   context "タイトルが空白のとき" do
-#     it "記事の作成に失敗する" do
-#       article = build(:article, title: nil)
-#       expect(article).not_to be_valid
-#     end
-#   end
+  context "タイトルが空白のとき" do
+    it "記事の作成に失敗する" do
+      article = build(:article, title: nil)
+      expect(article).not_to be_valid
+    end
+  end
 
-#   context "本文が空白のとき" do
-#     it "記事の作成に失敗する" do
-#       article = build(:article, body: nil)
-#       expect(article).not_to be_valid
-#     end
-#   end
-# end
+  context "本文が空白のとき" do
+    it "記事の作成に失敗する" do
+      article = build(:article, body: nil)
+      expect(article).not_to be_valid
+    end
+  end
+end
