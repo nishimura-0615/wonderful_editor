876f97fecfe90abdefc70af406837b667a872d1a
nishimura-0615 <nsmr0615kenta@gmail.com>
2023-02-04 00:37:01 +0900

    articles_controller.rbにcreateメソッドを定義

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,12 +2,23 @@ module Api::V1
   class ArticlesController < BaseApiController
     def index
       articles = Article.all
-      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
+      render json: articles, each_serializer: Api::V1::ArticleSerializer
     end
 
     def show
       article = Article.find(params[:id])
-      render json: article, serializer: Api::V1::ArticlePreviewSerializer
+      render json: article, serializer: Api::V1::ArticleSerializer
     end
+
+    def create
+      article = current_user.articles.create!(article_params)
+      render json: article, serializer: Api::V1::ArticleSerializer
+    end
+
+    private
+
+      def article_params
+        params.require(:article).permit(:title, :body)
+      end
   end
 end
