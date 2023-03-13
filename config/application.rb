a808bf9b3610e16e757f6d2aae8e9f6a2915b540
西村健太 <>
2023-03-13 22:47:57 +0900

    エラー対策の記述を行う

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module WonderfulEditor
     end
 
     config.api_only = true
+    config.middleware.use ActionDispatch::Flash
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
